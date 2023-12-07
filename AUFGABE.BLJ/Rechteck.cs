a9cb26d9c00d923792a788479080f0aa5a05ef44
Riccardo <riccardo.greco@xappido.com>
2023-12-07 09:33:20 +0100

    Projektdateien hinzufügen.

---
+++ AUFGABE.BLJ/Rechteck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AUFGABE.BLJ
+{
+    internal class Rechteck
+    {
+
+        public double height {  get; set; }
+        public double width { get; set; }
+       
+    }
+}
